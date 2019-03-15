bed300efdcdede9c95990f856fbbd7b4ffed682f
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2019-03-15 21:36:17 +0900

    step: (fun x -> x) (fun _ -> f ()) (f ())

--- tests/unit/stepper.spec.js
+++ tests/unit/stepper.spec.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
 import * as stepper from '@/ocha/stepper.js'
+import * as parser from '@/ocha/parser.js'
 import * as ast from '@/ocha/ast-utils.js'
 import * as printer from '@/ocha/printer.js'
 
@@ -15,11 +16,15 @@ describe('stepper.js', () => {
     expect(printer.printExpr(stepper.step([], target))).to.equal('3');
   })
 
-  it('should step arg.', () => {
-    const f = ast.funDef('f', [ast.astUnit()], 10);
-    const target = ast.funApp(ast.lambdaExpr(['x'], 'x'), [ast.funApp('f', [ast.astUnit()])]);
-    expect(printer.printExpr(stepper.step([f], target))).to.equal('(fun x -> x) ((fun () -> 10) ())');
-  })
+  for (const [target, expected] of [
+    ['(fun x -> x) (f ())', '(fun x -> x) ((fun () -> 10) ())'],
+    ['(fun x -> x) (fun _ -> f ()) (f ())', '(fun x -> x) (fun _ -> f ()) ((fun () -> 10) ())'],
+  ]) {
+    it('should step arg.', () => {
+      const p = parser.parse('let f () = 10;;' + target + ';;');
+      expect(printer.printExpr(stepper.step(p.defs, p.expr))).to.equal(expected);
+    })
+  }
 
   it('should step simple reset.', () => {
     const target = ast.reset(3);
